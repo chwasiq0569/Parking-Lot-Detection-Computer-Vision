a3aafd87b2f3cfff3099f9589a08565be9343a5d
wasiq abdullah <wasiqabdullah222@gmail.com>
2023-10-04 18:59:27 +0500

    Added Polylines around PArked cars

--- car_detection.py
+++ car_detection.py
@@ -15,6 +15,14 @@ cap = cv2.VideoCapture("./easy1.mp4")
 
 count = 0
 
+
+def is_point_inside_polygon(point, polygon):
+    # Check if a point is inside a polygon
+    x, y = point
+    polygon = np.array(polygon)
+    return cv2.pointPolygonTest(polygon, (x, y), False) >= 0
+
+
 polylines = []
 area_names = []
 try:
@@ -41,9 +49,8 @@ while True:
     results = model.predict(frame)
     a = results[0].boxes.data
     px = pd.DataFrame(a).astype("float")
-
+    list1 = []
     for index, row in px.iterrows():
-
         x1 = int(row[0])
         y1 = int(row[1])
         x2 = int(row[2])
@@ -55,11 +62,21 @@ while True:
         cy = int(y1+y2)//2
 
         if 'car' in c:
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 255, 255), 2)
+            list1.append([cx, cy])
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 255, 255), 1)
 
     for i, polyline in enumerate(polylines):
-        cv2.polylines(frame, [polyline], True, (0, 0, 255), 2)
+        cv2.polylines(frame, [polyline], True, (0, 255, 0), 1)
         cvzone.putTextRect(frame, f'{area_names[i]}', tuple(polyline[0]), 1, 1)
+
+        for il in list1:
+            cx1 = il[0]
+            cy1 = il[1]
+            result = is_point_inside_polygon((cx1, cy1), polyline)
+            if result:
+                cv2.circle(frame, (cx1, cy1), 5, (255, 0, 0), -1)
+                cv2.polylines(frame, [polyline], True, (255, 0, 0), 2)
+
     cv2.imshow('FRAME', frame)
     key = cv2.waitKey(1) & 0xFF
 
