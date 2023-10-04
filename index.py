cbbd7f63f267f3f7d9269f8ff80018c1f170ea81
wasiq abdullah <wasiqabdullah222@gmail.com>
2023-10-04 11:39:59 +0500

    Added remove polygon function

--- index.py
+++ index.py
@@ -8,10 +8,17 @@ points = []
 polylines = []
 
 
+def is_point_inside_polygon(point, polygon):
+    # Check if a point is inside a polygon
+    x, y = point
+    polygon = np.array(polygon)
+    return cv2.pointPolygonTest(polygon, (x, y), False) >= 0
+
+
 def draw(event, x, y, flags, param):
-    global points, drawing
-    drawing = True
+    global points, drawing, polylines
     if event == cv2.EVENT_LBUTTONDOWN:
+        drawing = True
         points = [(x, y)]
     elif event == cv2.EVENT_MOUSEMOVE:
         if drawing:
@@ -19,7 +26,12 @@ def draw(event, x, y, flags, param):
     elif event == cv2.EVENT_LBUTTONUP:
         drawing = False
         polylines.append(np.array(points, np.int32))
-    print(polylines)
+    elif event == cv2.EVENT_LBUTTONDBLCLK:
+        # Check if the user double-clicked inside any polygon and remove it
+        for i, polyline in enumerate(polylines):
+            if is_point_inside_polygon((x, y), polyline):
+                del polylines[i]
+                break
 
 
 while True:
@@ -33,8 +45,10 @@ while True:
         cv2.polylines(frame, [polyline], True, (0, 0, 255), 2)
 
     cv2.imshow('FRAME', frame)
-    cv2.setMouseCallback('FRAME',  draw)
+    cv2.setMouseCallback('FRAME', draw)
     key = cv2.waitKey(100) & 0xFF
+    if key == ord('q'):
+        break
 
 cap.release()
 cv2.destroyAllWindows()
